5ae9d0f0c89c36b03533f5057e7d81c09360b5c7
Felipe Henrich <henrich.felipe@gmail.com>
2025-06-29 15:08:20 -0300

    style: fix last button text wrapping

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -19,7 +19,7 @@ export function PageButton({
       <button
         onClick={() => setActivePageId(id!)}
         className={cn([
-          "relative z-2 flex cursor-pointer items-center gap-2 rounded-md bg-[rgb(236,238,241)] px-[10px] py-1 text-[#677289] outline-1 transition delay-100 duration-200 ease-in-out hover:scale-102 hover:bg-[rgb(217,220,225)] focus:bg-[#f9fafb] focus:text-[#1A1A1A] focus:shadow focus:*:first:text-[#f59d0e] focus-visible:outline-[#2F72E2]",
+          "relative z-2 flex cursor-pointer items-center gap-2 rounded-md bg-[rgb(236,238,241)] px-[10px] py-1 text-nowrap text-[#677289] outline-1 transition delay-100 duration-200 ease-in-out hover:scale-102 hover:bg-[rgb(217,220,225)] focus:bg-[#f9fafb] focus:text-[#1A1A1A] focus:shadow focus:*:first:text-[#f59d0e] focus-visible:outline-[#2F72E2]",
           id === activePageId &&
             "z-1 bg-[#f9fafb] text-[#1A1A1A] shadow *:first:text-[#f59d0e]",
           className,
